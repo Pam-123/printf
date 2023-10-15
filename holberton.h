d93dbf0501219788f45532a3a091c9565c1e639a
Pam-123 <pamschawira@gmail.com>
2023-10-15 04:33:02 -0700

    WO

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_integer(int num);
+int _print_string(char *str);
+
+#endif /* HOLBERTON_H */
