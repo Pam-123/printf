d7cdbc21332442379dbcd893be1551f86a292c93
Pam-123 <pamschawira@gmail.com>
2023-10-17 08:23:49 -0700

    Push

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include "main.h"
+#include <limits.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -13,9 +14,9 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("Length:[%d, %i]\n", len, len);
+	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-	printf("Negative:[%d]\n", -762534);
+	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
