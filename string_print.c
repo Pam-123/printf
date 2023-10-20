c1251c9f392eb7919bf77e934e1a7189e4d631c6
Pam-123 <pamschawira@gmail.com>
2023-10-20 07:47:34 -0700

    Printf

--- string_print.c
+++ string_print.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
 /**
- * string_print - prints a string
- * @args: argument
- *
- * Return: Length
+ * string_print - a function that print the string
+ * @final_args: argument
+ * Return: the length of the string
  */
-int string_print(va_list args)
-{
-	char *string;
-	int s;
-	int span;
 
-	string = va_arg(args, char *);
-	if (string == NULL)
+int string_print(va_list final_args)
+{
+	int w;
+	char *stri;
+
+	stri = va_arg(final_args, char *);
+	if (stri == NULL)
+		stri = "(null)";
+	for (w = 0; stri[w] != '\0'; w++)
 	{
-		string = "(null)";
-		span = _strlen(string);
-		for (s = 0; s < span; s++)
-		{
-			op_putchar(string[s]);
-		}
-		return (span);
+		op_putchar(stri[w]);
 	}
-	else
-	{
-		span = _strlen(string);
-		for (s = 0; s < span; s++)
-		{
-			op_putchar(string[s]);
-		}
-		return (span);
-	}
+	return (w);
 }
